af1e84177a3c25969b998a91121ae0b2e2cfef2d
Ali Tavallaie <a.tavallaie@gmail.com>
2024-08-17 14:43:37 +0330

    test(daisy_ui): Update button component tests to account for default type attribute
    - Updated tests to include  attribute in the expected HTML.
    - Ensured that the DaisyUIButton class produces the expected output when using different variants, sizes, and modifiers.

--- tests/daisy_ui/test_button.py
+++ tests/daisy_ui/test_button.py
@@ -10,9 +10,7 @@ from trunco.daisy_ui.button import (
 class TestDaisyUIButton(unittest.TestCase):
     def test_button_default(self):
         button = DaisyUIButton(label="Click Me")
-        expected_html = (
-            f'<button id="{button.id}" class="btn btn-primary btn-md">Click Me</button>'
-        )
+        expected_html = f'<button id="{button.id}" class="btn btn-primary btn-md" type="button">Click Me</button>'
         self.assertEqual(str(button), expected_html)
 
     def test_button_with_variant_and_size(self):
@@ -21,9 +19,7 @@ class TestDaisyUIButton(unittest.TestCase):
             variant=DaisyUIButtonVariant.SUCCESS,
             size=DaisyUIButtonSize.LARGE,
         )
-        expected_html = (
-            f'<button id="{button.id}" class="btn btn-success btn-lg">Click Me</button>'
-        )
+        expected_html = f'<button id="{button.id}" class="btn btn-success btn-lg" type="button">Click Me</button>'
         self.assertEqual(str(button), expected_html)
 
     def test_button_with_modifiers(self):
@@ -33,14 +29,14 @@ class TestDaisyUIButton(unittest.TestCase):
             size=DaisyUIButtonSize.SMALL,
             modifiers=[DaisyUIButtonModifier.OUTLINE, DaisyUIButtonModifier.GLASS],
         )
-        expected_html = f'<button id="{button.id}" class="btn btn-warning btn-sm btn-outline glass">Click Me</button>'
+        expected_html = f'<button id="{button.id}" class="btn btn-warning btn-sm btn-outline glass" type="button">Click Me</button>'
         self.assertEqual(str(button), expected_html)
 
     def test_button_with_additional_classes(self):
         button = DaisyUIButton(
             label="Click Me", additional_classes=["custom-class", "another-class"]
         )
-        expected_html = f'<button id="{button.id}" class="btn btn-primary btn-md custom-class another-class">Click Me</button>'
+        expected_html = f'<button id="{button.id}" class="btn btn-primary btn-md custom-class another-class" type="button">Click Me</button>'
         self.assertEqual(str(button), expected_html)
 
 
